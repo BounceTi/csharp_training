67e3d4826c6040ca9e90f595c7f85cb6c3db7d9d
AnatolyDenisenko <na95to@gmail.com>
2022-04-08 18:34:57 +0700

    Ex. 18 Realizing tests for adding and removing groups

--- addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace addressbook_tests_autoit
 {
     public class GroupHelper : HelperBase
     {
         public static string GROUPWINTITLE = "Group editor";
+        public static string GROUPDELETEWINTITLE = "Delete group";
         public GroupHelper(ApplicationManager manager) : base(manager) { }
 
         public List<GroupData> GetGroupList()
@@ -26,6 +28,15 @@ namespace addressbook_tests_autoit
             CloseGroupsDialogue();
             return list;
         }
+        public bool CheckGroupExist()
+        {
+            OpenGroupsDialogue();
+            if (GetGroupList().Count() <= 1)
+            {
+                return false;
+            }
+            return true;
+        }
 
         public void Add(GroupData newGroup)
         {
@@ -36,6 +47,28 @@ namespace addressbook_tests_autoit
             CloseGroupsDialogue();
         }
 
+        public void Remove(GroupData toBeRemoved)
+        {
+            OpenGroupsDialogue();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "GetText", "#0|#" + i, "");
+                if (item.Equals(toBeRemoved.Name))
+                {
+                    aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51",
+                "Select", "#0|#" + i, "");
+                    break;
+                }
+            }
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");
+            aux.WinWait(GROUPDELETEWINTITLE);
+            aux.ControlClick(GROUPDELETEWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            CloseGroupsDialogue();
+        }
+
         private void CloseGroupsDialogue()
         {
             aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
@@ -43,6 +76,10 @@ namespace addressbook_tests_autoit
 
         private void OpenGroupsDialogue()
         {
+            if (aux.WinGetTitle("[active]") == GROUPWINTITLE)
+            {
+                return;
+            }
             aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
             aux.WinWait(GROUPWINTITLE);
         }
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            if (!app.Groups.CheckGroupExist())
+            {
+                GroupData group = new GroupData()
+                {
+                    Name = "Name"
+                };
+
+                app.Groups.Add(group);
+            }
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData toBeRemoved = oldGroups[0];
+
+            app.Groups.Remove(toBeRemoved);
+
+            Assert.AreEqual(oldGroups.Count - 1, app.Groups.GetGroupList().Count());
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            oldGroups.RemoveAt(0);
+            Assert.AreEqual(oldGroups, newGroups);
+
+            foreach (GroupData group in newGroups)
+            {
+                Assert.AreNotEqual(group.Name, toBeRemoved.Name);
+
+            }
+        }
+    }
+}
